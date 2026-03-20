4157b5375b4080c74a6061ee801b8cf92cbc6f4f
infobesttoolsmail882 <info@besttoolsmail.online>
2026-03-20 13:36:12 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = 8080;
 
 /* ================= CONFIG ================= */
 
-const ADMIN_LOGIN = "@##2588^$$^O^%%^@#";
+const ADMIN_LOGIN = "#$#nikhil@#$#$#$";
 
 const SESSION_SECRET = crypto.randomBytes(32).toString("hex");
 const SESSION_TIME = 60 * 60 * 1000;
